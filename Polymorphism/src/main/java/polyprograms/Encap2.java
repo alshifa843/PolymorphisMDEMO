41a75b625bdb54cd935f1463423f3e7e8279acf3
alshifaalshu <alshifahussain182@gmail.com>
2024-10-01 11:14:01 +0530

    sample encapsulation

---
+++ Polymorphism/src/main/java/polyprograms/Encap2.java
@@ -0,0 +1,16 @@
+package polyprograms;
+
+public class Encap2 {
+
+	public static void main(String[] args) {
+		Encap obj = new Encap();
+		obj.setName("ramu");
+		System.out.println(obj.getName());
+		obj.setAge(34);
+		System.out.println(obj.getAge());
+		obj.setSalary(3456);
+		System.out.println(obj.getSalary());
+
+	}
+
+}
