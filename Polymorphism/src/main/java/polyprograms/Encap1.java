8a3e64f2a1a43f705b16573e9fac5ef8f29eddc0
alshifaalshu <alshifahussain182@gmail.com>
2024-10-01 11:25:41 +0530

    samples of encapsulation

---
+++ Polymorphism/src/main/java/polyprograms/Encap1.java
@@ -0,0 +1,21 @@
+package polyprograms;
+
+public class Encap1 {
+	
+	private String val ="ammu";
+	private int age = 23;
+	
+	public String getVal() {
+		return val;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	
+	}
+	
+	
+
+
---
+++ Polymorphism/src/main/java/polyprograms/Encap1Child.java
@@ -0,0 +1,12 @@
+package polyprograms;
+
+public class Encap1Child {
+
+	public static void main(String[] args) {
+		Encap1 obj = new Encap1();
+		System.out.println(obj.getVal());
+		System.out.println(obj.getAge());
+
+	}
+
+}
